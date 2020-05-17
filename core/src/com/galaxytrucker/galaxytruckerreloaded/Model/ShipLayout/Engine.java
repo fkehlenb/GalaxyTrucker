b133029fb5e7c918a499dd2b126ab725ba32285f
lnittsch <lnittsch@uni-bremen.de>
2020-05-17 18:33:00 +0200

    fixed oopsie

--- core/src/com/galaxytrucker/galaxytruckerreloaded/Model/ShipLayout/Engine.java
+++ core/src/com/galaxytrucker/galaxytruckerreloaded/Model/ShipLayout/Engine.java
@@ -1,6 +1,6 @@
 package com.galaxytrucker.galaxytruckerreloaded.Model.ShipLayout;
 
-public class Engine extends System implements {
+public class Engine extends System {
     /** Ob ein Crewmitglied das System stärkt */
     private boolean manned;
 }
