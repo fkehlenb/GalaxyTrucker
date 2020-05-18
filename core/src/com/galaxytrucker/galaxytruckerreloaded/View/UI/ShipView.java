0dec34d2715fd261577c09bb4947a157ea0355c3
Leonard <Leonard@Leonard.Leo>
2020-05-18 19:48:36 +0300

    added ship view

---
+++ core/src/com/galaxytrucker/galaxytruckerreloaded/View/UI/ShipView.java
@@ -0,0 +1,74 @@
+package com.galaxytrucker.galaxytruckerreloaded.View.UI;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.galaxytrucker.galaxytruckerreloaded.Main;
+import com.galaxytrucker.galaxytruckerreloaded.Model.Ship;
+
+public class ShipView {
+
+    /**
+     * SpriteBatch
+     */
+    private SpriteBatch batch;
+
+    /**
+     * Orthographic camera
+     */
+    private OrthographicCamera camera;
+
+    /**
+     * Ship used by this view
+     */
+    private Ship ship;
+
+    /**
+     * Background image if its the enemy ship
+     */
+    private Texture enemyShipBackgroundTexture;
+
+    /**
+     * Setup called after initialisation
+     */
+    private void setup() {
+    }
+
+    /**
+     * Show the ship
+     */
+    public void showShipView() {
+    }
+    
+    /**
+     * Hide the ship
+     */
+    public void hideShipView() {
+    }
+
+    /**
+     * Dispose of ship
+     */
+    public void disposeShipView() {
+    }
+
+    /**
+     * Ship hop animation
+     */
+    private void hyperspeedHopAnimation() {
+    }
+
+    /**
+     * Shield destroyed animation
+     */
+    private void shipDestroyedAnimation() {
+    }
+
+    /**
+     * Constructor
+     *
+     * @param main - the main class for SpriteBatch
+     */
+    public ShipView(Main main) {
+    }
+}
