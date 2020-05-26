e8381122f440b25b72c54f5c228a1299dea3e6a6
Leonard <Leonard@Leonard.Leo>
2020-05-26 18:53:54 +0300

    server changes

--- core/src/com/galaxytrucker/galaxytruckerreloaded/Server/Server.java
+++ core/src/com/galaxytrucker/galaxytruckerreloaded/Server/Server.java
@@ -2,12 +2,18 @@ package com.galaxytrucker.galaxytruckerreloaded.Server;
 
 import com.galaxytrucker.galaxytruckerreloaded.Server.Database.Database;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 /**
  * This class creates the game server and handles storing the data
  */
 public class Server {
 
-    /** The database */
+    /**
+     * The database
+     */
     private Database database;
 
     /**
@@ -19,15 +25,26 @@ public class Server {
     }
 
     /**
-     * Send some data
+     * Client handler handles clients connected
      */
-    public void sendPackets() {
+    public void client_handler(Socket socket) {
+    }
+
+    /**
+     * Send some data
+     *
+     * @param stream - input stream of data to send
+     */
+    public void sendPackets(InputStream stream) {
     }
 
     /**
      * Receive some data
+     *
+     * @return outputstream of received data
      */
-    public void receivePackets() {
+    public OutputStream receivePackets() {
+        return null;
     }
 
     /**
