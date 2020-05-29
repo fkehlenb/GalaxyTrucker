2786393a61c02eb3759d59aeffdc91cf48884c0f
Leonard <Leonard@Leonard.Leo>
2020-05-29 19:08:03 +0300

    added hangar controller, added field in overworld

--- core/src/com/galaxytrucker/galaxytruckerreloaded/Server/Services/HangarService.java
+++ core/src/com/galaxytrucker/galaxytruckerreloaded/Server/Services/HangarService.java
@@ -1,14 +1,32 @@
 package com.galaxytrucker.galaxytruckerreloaded.Server.Services;
 
-import java.util.UUID;
+import com.galaxytrucker.galaxytruckerreloaded.Model.Map.Overworld;
+import com.galaxytrucker.galaxytruckerreloaded.Model.Ship;
 
-/** Handles picking a ship, creating the game map and so on */
+/**
+ * Handles picking a ship, creating the game map and so on
+ */
 public class HangarService {
 
-    /** Generate a map
+    /**
+     * Generate a map
+     *
      * @param username - the user who to generate a map for
-     * @param seed - seed for map generation */
-    public void generateMap(String username,int seed){}
+     * @param seed     - seed for map generation
+     * @return the generated map
+     */
+    public Overworld generateMap(String username, int seed) {
+        return null;
+    }
 
-    /** */
+    /**
+     * Make the user pick a ship
+     *
+     * @param username   - the user who wants to pick a ship
+     * @param shipDesign - the ship design the user chose
+     * @return a new ship object for that design
+     */
+    public Ship chooseShip(String username, String shipDesign) {
+        return null;
+    }
 }
