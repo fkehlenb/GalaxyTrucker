9fea4870bf9b3b40b549d23def1845f9ef7bce68
Fabian <fakehlenbeck@googlemail.com>
2020-05-29 18:21:13 +0200

    SystemController add

---
+++ core/src/com/galaxytrucker/galaxytruckerreloaded/Controller/Actions/SystemController.java
@@ -0,0 +1,37 @@
+package com.galaxytrucker.galaxytruckerreloaded.Controller.Actions;
+
+import com.galaxytrucker.galaxytruckerreloaded.Model.Ship;
+import com.galaxytrucker.galaxytruckerreloaded.Model.ShipLayout.Room;
+import com.galaxytrucker.galaxytruckerreloaded.Model.ShipLayout.System;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
+public class SystemController extends Controller {
+    /** My own ship*/
+    private Ship myself;
+
+    /**
+     * Install a new system on a ship
+     *
+     * @param system - the system to install
+     * @param room   - the room to install the system in
+     */
+    public void installSystem(System system, Room room) {
+
+    }
+
+    /**
+     * Uninstall a system on a ship
+     *
+     * @param system - the system to remove
+     */
+    public void uninstalledSystem(System system) {
+
+    }
+
+}
