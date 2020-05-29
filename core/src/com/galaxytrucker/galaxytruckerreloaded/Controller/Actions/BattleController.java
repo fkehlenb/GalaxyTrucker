6cd1a002898b9dbda274824e67346aac7a78832d
Fabian <fakehlenbeck@googlemail.com>
2020-05-29 16:15:10 +0200

    Battle Controller

---
+++ core/src/com/galaxytrucker/galaxytruckerreloaded/Controller/Actions/BattleController.java
@@ -0,0 +1,28 @@
+package com.galaxytrucker.galaxytruckerreloaded.Controller.Actions;
+
+import com.galaxytrucker.galaxytruckerreloaded.Model.Ship;
+import com.galaxytrucker.galaxytruckerreloaded.Model.Weapons.Weapon;
+import lombok.*;
+
+@Getter
+@Setter
+@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
+public class BattleController extends Controller{
+    @NonNull
+    private Ship myself;
+
+    /** Make one ship attack another's section
+     * @param opponent - the opponent's ship
+     * @param weapon - the weapon attacking */
+    private void attack(Ship opponent, Weapon weapon){}
+
+    /** Heal a ship
+     * @param healingWeapon - the healing weapon */
+    private void heal(Weapon healingWeapon){}
+
+    /** Flee a fight, reward the winner
+     * @param coward - the ship that wants to flee
+     * @param opponent - the opponent that wins the fight */
+    private void fleeFight(Ship coward, Ship opponent){}
+
+}
--- core/src/com/galaxytrucker/galaxytruckerreloaded/Controller/Actions/Controller.java
+++ core/src/com/galaxytrucker/galaxytruckerreloaded/Controller/Actions/Controller.java
@@ -1,9 +1,12 @@
 package com.galaxytrucker.galaxytruckerreloaded.Controller.Actions;
 
+import lombok.NonNull;
+
 public abstract class Controller {
 
     /** ClientControllerCommunicator */
+    @NonNull
     private ClientControllerCommunicator clientControllerCommunicator;
 
-    // Extend me
+
 }
