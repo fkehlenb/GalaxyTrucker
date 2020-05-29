a9d20c3d9d4367d14e6d77334f4ef565ee3ca4f3
Leonard <Leonard@Leonard.Leo>
2020-05-29 19:14:33 +0300

    added system service and added incombat field to ship

--- core/src/com/galaxytrucker/galaxytruckerreloaded/Server/Services/SystemService.java
+++ core/src/com/galaxytrucker/galaxytruckerreloaded/Server/Services/SystemService.java
@@ -1,9 +1,12 @@
 package com.galaxytrucker.galaxytruckerreloaded.Server.Services;
 
+import com.galaxytrucker.galaxytruckerreloaded.Model.Ship;
+import com.galaxytrucker.galaxytruckerreloaded.Model.ShipLayout.Room;
 import com.galaxytrucker.galaxytruckerreloaded.Model.ShipLayout.System;
 import com.galaxytrucker.galaxytruckerreloaded.Server.Exception.DuplicateRoomException;
 import com.galaxytrucker.galaxytruckerreloaded.Server.Exception.RoomNotFoundException;
 import com.galaxytrucker.galaxytruckerreloaded.Server.Persistence.RoomDAO;
+import com.galaxytrucker.galaxytruckerreloaded.Server.Persistence.ShipDAO;
 
 public class SystemService {
 
@@ -13,20 +16,38 @@ public class SystemService {
     private RoomDAO roomDAO;
 
     /**
-     * Add a new system
-     *
-     * @param s - the system to add
-     * @throws DuplicateRoomException if the system already exists
+     * Ship DAO
      */
-    public void addSystem(System s) throws DuplicateRoomException {
+    private ShipDAO shipDAO;
+
+    /**
+     * Validate system install/uninstall
+     *
+     * @param s - the command
+     * @return true if it is valid, else false
+     */
+    public boolean validateSystemReplacement(String s) {
+        return false;
     }
 
     /**
-     * Remove a system
+     * Install a new system on a ship
      *
-     * @param s - the system to remove
-     * @throws RoomNotFoundException if the system cannot be found
+     * @param ship   - the ship to install the system on
+     * @param system - the system to install
+     * @param room   - the room to install the system in
      */
-    public void removeSystem(System s) throws RoomNotFoundException {
+    public void installSystem(Ship ship, System system, Room room) {
+
+    }
+
+    /**
+     * Uninstall a system on a ship
+     *
+     * @param ship   - the ship to remove the system from
+     * @param system - the system to remove
+     */
+    public void uninstalledSystem(Ship ship, System system) {
+
     }
 }
