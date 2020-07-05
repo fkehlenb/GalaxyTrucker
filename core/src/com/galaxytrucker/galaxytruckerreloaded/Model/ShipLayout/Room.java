873c93dfe8339afb61f37c646b59ba9425221c29
Leonard <Leonard@Leonard.Leo>
2020-07-05 18:52:49 +0300

    added tiles

--- core/src/com/galaxytrucker/galaxytruckerreloaded/Model/ShipLayout/Room.java
+++ core/src/com/galaxytrucker/galaxytruckerreloaded/Model/ShipLayout/Room.java
@@ -4,13 +4,11 @@ package com.galaxytrucker.galaxytruckerreloaded.Model.ShipLayout;
 import com.galaxytrucker.galaxytruckerreloaded.Model.Crew.Crew;
 import lombok.*;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
+/** Rooms make up the spaceship */
 @Getter
 @Setter
 @Entity
@@ -60,4 +58,9 @@ public abstract class Room implements Serializable {
     @NonNull
     @OneToMany(cascade = CascadeType.ALL)
     private List<Crew> crew;
+
+    /** Tiles the room is made out of */
+    @NonNull
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Tile> tiles;
 }
---
+++ core/src/com/galaxytrucker/galaxytruckerreloaded/Model/ShipLayout/Tile.java
@@ -0,0 +1,39 @@
+package com.galaxytrucker.galaxytruckerreloaded.Model.ShipLayout;
+
+import com.galaxytrucker.galaxytruckerreloaded.Model.Crew.Crew;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.io.Serializable;
+
+/** Tiles make up the rooms */
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
+@Getter
+@Setter
+@Entity
+public class Tile implements Serializable {
+
+    /** ID */
+    @Id @NonNull
+    private int id;
+
+    /** Position x in room */
+    @NonNull
+    private int posX;
+
+    /** Position y in room */
+    @NonNull
+    private int posY;
+
+    /** Crew member on this tile */
+    @OneToOne
+    private Crew standingOnMe = null;
+
+    /** If the tile is empty */
+    public boolean isEmpty(){
+        return standingOnMe != null;
+    }
+}
