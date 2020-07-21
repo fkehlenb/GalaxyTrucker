92d8b550a11c8dd3c3dc10853e0740394528fe05
Fabian <fakehlenbeck@googlemail.com>
2020-07-21 15:40:09 +0200

    MapUi fixes NICHT FERTIG

--- core/src/com/galaxytrucker/galaxytruckerreloaded/View/UI/Map/MapUI.java
+++ core/src/com/galaxytrucker/galaxytruckerreloaded/View/UI/Map/MapUI.java
@@ -65,16 +65,63 @@ public class MapUI {
         x = Main.WIDTH/2f - mapTexture.getWidth()/2f;
         y = Main.HEIGHT/2f - mapTexture.getHeight()/2f;
 
-        closeButton = new InventoryCloseButton(x+(Main.WIDTH/2.01f), y-(Main.HEIGHT/13.5f), Main.WIDTH/7.742f, Main.HEIGHT/21.6f, null, null, this);        stage.addActor(closeButton);
+        closeButton = new InventoryCloseButton(x+(Main.WIDTH/2.01f), y-(Main.HEIGHT/13.5f), Main.WIDTH/7.742f, Main.HEIGHT/21.6f, null, null, this);
+        stage.addActor(closeButton);
 
         locations = new LinkedList<>();
         //TODO
         for(Planet f : map.getPlanetMap()) {
             float fx = f.getPosX();
             float fy = f.getPosY();
-           MapButton mb = new MapButton(new Texture("map/map_button.png"), (x+fx+Main.WIDTH/(1920/120)), (y+fy+Main.HEIGHT/(1080/80)), Main.WIDTH/(1920/20), Main.HEIGHT/(1080/20), this, f);
-           locations.add(mb);
-            stage.addActor(mb);
+
+            //int mapSize = map.getPlanetMap().size()*2;
+            //StartPlanet Extra
+            if(fx == -1 && fy == -1){
+                 //Start
+                MapButton mb = new MapButton(new Texture("map/map_button.png"), (x+fx+Main.WIDTH/(1920/120)), (y+fy+Main.HEIGHT/(1080/95)), Main.WIDTH/(1920/20), Main.HEIGHT/(1080/20), this, f);
+                locations.add(mb);
+                stage.addActor(mb);
+            }
+            else if (fx == 30 && fy == 30){
+                //Boss
+                MapButton mb = new MapButton(new Texture("map/map_button.png"), (x+fx+Main.WIDTH/(1920/120)), (y+fy+Main.HEIGHT/(1080/95)), Main.WIDTH/(1920/20), Main.HEIGHT/(1080/20), this, f);
+                locations.add(mb);
+                stage.addActor(mb);
+            }
+            else {
+                //Alle anderen
+                MapButton mb = new MapButton(new Texture("map/map_button.png"), (x+fx*Main.WIDTH/(1920/100)+Main.WIDTH/(1920/150)), (y+fy*Main.HEIGHT/(1080/50)), Main.WIDTH/(1920/20), Main.HEIGHT/(1080/20), this, f);
+                locations.add(mb);
+                stage.addActor(mb);
+            }
+
+
+          //Main.HEIGHT/22 ... Main.WIDTH/40
+          //Switch-Case für die PlanetEvents (Map Texturen)
+            /*  switch(f.getEvent()){
+
+                case VOID:
+
+                case COMBAT:
+
+                case SHOP:
+
+                case NEBULA:
+
+                case MINIBOSS:
+
+                case METEORSHOWER:
+
+                case PVP:
+
+                case BOSS:
+
+            }*/
+
+
+
+           //locations.add(mb);
+            //stage.addActor(mb);
         }
     }
 
