a31affa1b544d4e5b82320f28ad6870aae7a6958
Leonard <Leonard@Leonard.Leo>
2020-07-23 22:52:24 +0300

    ai synchronisation fix

--- core/src/com/galaxytrucker/galaxytruckerreloaded/Server/Opponent/NormalAI.java
+++ core/src/com/galaxytrucker/galaxytruckerreloaded/Server/Opponent/NormalAI.java
@@ -57,135 +57,133 @@ public class NormalAI {
      * @param responseObject - responseObject containing the last move
      * @param weapons        - the ship's weapons
      */
-    private void learn(ResponseObject responseObject, List<Weapon> weapons) {
+    private synchronized void learn(ResponseObject responseObject, List<Weapon> weapons) {
         try {
-            synchronized (targets) {
-                if (responseObject.getPreviousRoundAction() != null) {
-                    if (previousCrewHealth != null) {
-                        List<Crew> crew = new ArrayList<>();
-                        for (Room r : responseObject.getResponseShip().getSystems()) {
-                            crew.addAll(r.getCrew());
-                        }
-                        crewHealth = new int[crew.size()];
-                        for (int i = 0; i < crewHealth.length; i++) {
-                            crewHealth[i] = crew.get(i).getHealth();
-                        }
+            if (responseObject.getPreviousRoundAction() != null) {
+                if (previousCrewHealth != null) {
+                    List<Crew> crew = new ArrayList<>();
+                    for (Room r : responseObject.getResponseShip().getSystems()) {
+                        crew.addAll(r.getCrew());
                     }
-                    switch (responseObject.getPreviousRoundAction()) {
-                        case ATTACK_SHIP:
-                            if (previousCrewHealth != null && crewHealth != null && sum(previousCrewHealth) < sum(crewHealth)) {
-                                targets.add(TargetMode.CREW);
-                            } else {
-                                targets.add(TargetMode.WEAPON_SYSTEM);
-                            }
-                            targets.add(TargetMode.SHIELDS);
-                            break;
-                        case HEAL_CREW:
-                            targets.add(TargetMode.CREW);
-                            targets.add(TargetMode.O2);
-                            targets.add(TargetMode.MEDBAY);
-                            targets.add(TargetMode.ENGINE);
-                            break;
-                        case HEAL_CREW_IN_ROOM:
-                            targets.add(TargetMode.CREW);
-                            targets.add(TargetMode.O2);
-                            targets.add(TargetMode.SHIELDS);
-                            targets.add(TargetMode.MEDBAY);
-                            break;
-                        case MOVE_CREW:
-                            targets.add(TargetMode.CREW);
-                            targets.add(TargetMode.WEAPON_SYSTEM);
-                            targets.add(TargetMode.COCKPIT);
-                            break;
-                        case HEAL_SHIP:
-                            targets.add(TargetMode.CREW);
-                            targets.add(TargetMode.O2);
-                            targets.add(TargetMode.MEDBAY);
-                            targets.add(TargetMode.SHIELDS);
-                            targets.add(TargetMode.WEAPON_SYSTEM);
-                            targets.add(TargetMode.ENGINE);
-                            break;
-                        default:
-                            targets.add(TargetMode.WEAPON_SYSTEM);
-                            targets.add(TargetMode.SHIELDS);
-                            targets.add(TargetMode.COCKPIT);
-                            targets.add(TargetMode.ENGINE);
-                            targets.add(TargetMode.MEDBAY);
-                            break;
+                    crewHealth = new int[crew.size()];
+                    for (int i = 0; i < crewHealth.length; i++) {
+                        crewHealth[i] = crew.get(i).getHealth();
                     }
-                    for (Room r : responseObject.getOpponent().getSystems()) {
-                        if (r.getCrew().size() > 0) {
-                            if (r.isSystem() && (r.getBreach() <= 0 || !((System) r).isDisabled())) {
-                                SystemType systemType = ((System) r).getSystemType();
-                                switch (systemType) {
-                                    case MEDBAY:
-                                        targets.add(TargetMode.MEDBAY);
-                                        break;
-                                    case O2:
-                                        targets.add(TargetMode.O2);
-                                        break;
-                                    case ENGINE:
-                                        targets.add(TargetMode.ENGINE);
-                                        break;
-                                    case SHIELDS:
-                                        targets.add(TargetMode.SHIELDS);
-                                        break;
-                                    case CAMERAS:
-                                        targets.add(TargetMode.CAMERAS);
-                                        break;
-                                    case WEAPON_SYSTEM:
-                                        targets.add(TargetMode.WEAPON_SYSTEM);
-                                        break;
-                                    case COCKPIT:
-                                        targets.add(TargetMode.COCKPIT);
-                                        break;
-                                    default:
-                                        targets.add(TargetMode.ROOM);
-                                        break;
-                                }
-                            } else {
-                                targets.add(TargetMode.ROOM);
-                            }
-                        }
-                    }
-                    if (responseObject.getResponseShip().getShields() > 0) {
-                        targets.add(TargetMode.SHIELDS);
-                        targets.add(TargetMode.SHIELDS);
-                    } else {
-                        targets.add(TargetMode.COCKPIT);
-                        targets.add(TargetMode.ENGINE);
-                        targets.add(TargetMode.SHIELDS);
-                    }
-                    if (responseObject.getResponseShip().getFTLCharge() >= 50) {
-                        targets.add(TargetMode.ENGINE);
-                        targets.add(TargetMode.COCKPIT);
-                    }
-                    for (Weapon w : weapons) {
-                        switch (w.getWeaponType()) {
-                            case ROCKET:
-                                targets.add(TargetMode.SHIELDS);
-                                break;
-                            case LASER:
-                                targets.add(TargetMode.WEAPON_SYSTEM);
-                                targets.add(TargetMode.ENGINE);
-                                break;
-                            case RADIO:
-                                targets.add(TargetMode.CREW);
-                                break;
-                            case BOMB:
-                                targets.add(TargetMode.WEAPON_SYSTEM);
-                                targets.add(TargetMode.CREW);
-                                break;
-                            case RADIO_BOMB:
-                                targets.add(TargetMode.CREW);
-                                targets.add(TargetMode.COCKPIT);
-                                break;
-                        }
-                    }
-                    // Todo more intelligence
                 }
-                Collections.shuffle(targets);
+                switch (responseObject.getPreviousRoundAction()) {
+                    case ATTACK_SHIP:
+                        if (previousCrewHealth != null && crewHealth != null && sum(previousCrewHealth) < sum(crewHealth)) {
+                            targets.add(TargetMode.CREW);
+                        } else {
+                            targets.add(TargetMode.WEAPON_SYSTEM);
+                        }
+                        targets.add(TargetMode.SHIELDS);
+                        break;
+                    case HEAL_CREW:
+                        targets.add(TargetMode.CREW);
+                        targets.add(TargetMode.O2);
+                        targets.add(TargetMode.MEDBAY);
+                        targets.add(TargetMode.ENGINE);
+                        break;
+                    case HEAL_CREW_IN_ROOM:
+                        targets.add(TargetMode.CREW);
+                        targets.add(TargetMode.O2);
+                        targets.add(TargetMode.SHIELDS);
+                        targets.add(TargetMode.MEDBAY);
+                        break;
+                    case MOVE_CREW:
+                        targets.add(TargetMode.CREW);
+                        targets.add(TargetMode.WEAPON_SYSTEM);
+                        targets.add(TargetMode.COCKPIT);
+                        break;
+                    case HEAL_SHIP:
+                        targets.add(TargetMode.CREW);
+                        targets.add(TargetMode.O2);
+                        targets.add(TargetMode.MEDBAY);
+                        targets.add(TargetMode.SHIELDS);
+                        targets.add(TargetMode.WEAPON_SYSTEM);
+                        targets.add(TargetMode.ENGINE);
+                        break;
+                    default:
+                        targets.add(TargetMode.WEAPON_SYSTEM);
+                        targets.add(TargetMode.SHIELDS);
+                        targets.add(TargetMode.COCKPIT);
+                        targets.add(TargetMode.ENGINE);
+                        targets.add(TargetMode.MEDBAY);
+                        break;
+                }
+                for (Room r : responseObject.getOpponent().getSystems()) {
+                    if (r.getCrew().size() > 0) {
+                        if (r.isSystem() && (r.getBreach() <= 0 || !((System) r).isDisabled())) {
+                            SystemType systemType = ((System) r).getSystemType();
+                            switch (systemType) {
+                                case MEDBAY:
+                                    targets.add(TargetMode.MEDBAY);
+                                    break;
+                                case O2:
+                                    targets.add(TargetMode.O2);
+                                    break;
+                                case ENGINE:
+                                    targets.add(TargetMode.ENGINE);
+                                    break;
+                                case SHIELDS:
+                                    targets.add(TargetMode.SHIELDS);
+                                    break;
+                                case CAMERAS:
+                                    targets.add(TargetMode.CAMERAS);
+                                    break;
+                                case WEAPON_SYSTEM:
+                                    targets.add(TargetMode.WEAPON_SYSTEM);
+                                    break;
+                                case COCKPIT:
+                                    targets.add(TargetMode.COCKPIT);
+                                    break;
+                                default:
+                                    targets.add(TargetMode.ROOM);
+                                    break;
+                            }
+                        } else {
+                            targets.add(TargetMode.ROOM);
+                        }
+                    }
+                }
+                if (responseObject.getResponseShip().getShields() > 0) {
+                    targets.add(TargetMode.SHIELDS);
+                    targets.add(TargetMode.SHIELDS);
+                } else {
+                    targets.add(TargetMode.COCKPIT);
+                    targets.add(TargetMode.ENGINE);
+                    targets.add(TargetMode.SHIELDS);
+                }
+                if (responseObject.getResponseShip().getFTLCharge() >= 50) {
+                    targets.add(TargetMode.ENGINE);
+                    targets.add(TargetMode.COCKPIT);
+                }
+                for (Weapon w : weapons) {
+                    switch (w.getWeaponType()) {
+                        case ROCKET:
+                            targets.add(TargetMode.SHIELDS);
+                            break;
+                        case LASER:
+                            targets.add(TargetMode.WEAPON_SYSTEM);
+                            targets.add(TargetMode.ENGINE);
+                            break;
+                        case RADIO:
+                            targets.add(TargetMode.CREW);
+                            break;
+                        case BOMB:
+                            targets.add(TargetMode.WEAPON_SYSTEM);
+                            targets.add(TargetMode.CREW);
+                            break;
+                        case RADIO_BOMB:
+                            targets.add(TargetMode.CREW);
+                            targets.add(TargetMode.COCKPIT);
+                            break;
+                    }
+                }
+                // Todo more intelligence
             }
+            Collections.shuffle(targets);
         } catch (Exception e) {
             e.printStackTrace();
         }
