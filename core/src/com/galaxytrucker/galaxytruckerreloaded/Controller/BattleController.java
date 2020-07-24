25c561efa8c9fc0623ba467e418e3f55e252601f
Leonard <Leonard@Leonard.Leo>
2020-07-24 15:24:01 +0300

    added more options to controller

--- core/src/com/galaxytrucker/galaxytruckerreloaded/Controller/BattleController.java
+++ core/src/com/galaxytrucker/galaxytruckerreloaded/Controller/BattleController.java
@@ -4,10 +4,15 @@ import com.galaxytrucker.galaxytruckerreloaded.Communication.ClientControllerCom
 import com.galaxytrucker.galaxytruckerreloaded.Model.Ship;
 import com.galaxytrucker.galaxytruckerreloaded.Model.ShipLayout.Room;
 import com.galaxytrucker.galaxytruckerreloaded.Model.Weapons.Weapon;
+import com.galaxytrucker.galaxytruckerreloaded.Model.Weapons.WeaponType;
+import com.galaxytrucker.galaxytruckerreloaded.Server.PreviousRoundAction;
 import com.galaxytrucker.galaxytruckerreloaded.Server.RequestObject;
 import com.galaxytrucker.galaxytruckerreloaded.Server.RequestType;
 import com.galaxytrucker.galaxytruckerreloaded.Server.ResponseObject;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -41,6 +46,48 @@ public class BattleController extends Controller {
      */
     private ResponseObject previousResponse = null;
 
+    /** Previous round actions */
+    private List<PreviousRoundAction> previousRoundActions = new ArrayList<>();
+
+    /** Previous weapons used */
+    private List<WeaponType> previousWeaponsUsed = new ArrayList<>();
+
+    /** Check if the combat is over
+     * @return combat over */
+    public boolean combatOver(){
+        if (previousResponse!=null){
+            return previousResponse.isCombatOver();
+        }
+        return false;
+    }
+
+    /** Check if combat is won
+     * @return combat won */
+    public boolean combatWon(){
+        if (previousResponse!=null){
+            return previousResponse.isCombatWon();
+        }
+        return false;
+    }
+
+    /** Get the opponent's previous round actions
+     * @return previous round actions */
+    public List<PreviousRoundAction> getPreviousRoundActions(){
+        if (!previousRoundActions.isEmpty()){
+            return previousRoundActions;
+        }
+        return new ArrayList<>();
+    }
+
+    /** Get the weapons used by the opponent
+     * @return previous round weapons */
+    public List<WeaponType> getPreviousRoundWeapons(){
+        if (!previousWeaponsUsed.isEmpty()){
+            return previousWeaponsUsed;
+        }
+        return new ArrayList<>();
+    }
+
     /**
      * Play your round's moves
      */
@@ -69,11 +116,22 @@ public class BattleController extends Controller {
             clientControllerCommunicator.setClientShip(responseObject.getResponseShip());
             clientControllerCommunicator.setMap(responseObject.getResponseOverworld());
             previousResponse = responseObject;
+            if (responseObject.getPreviousRoundAction()!=null&&!responseObject.getPreviousRoundAction().isEmpty()){
+                previousRoundActions = responseObject.getPreviousRoundAction();
+            }
+            else{
+                previousRoundActions = new ArrayList<>();
+            }
+            if (responseObject.getWeaponUsed()!=null&&!responseObject.getWeaponUsed().isEmpty()){
+                previousWeaponsUsed = responseObject.getWeaponUsed();
+            }
+            else{
+                previousWeaponsUsed = new ArrayList<>();
+            }
             return true;
         }
         return false;
     }
-
 
     /**
      * Attack opponent
