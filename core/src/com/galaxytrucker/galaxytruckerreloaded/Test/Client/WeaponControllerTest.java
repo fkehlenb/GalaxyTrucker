f2c123a3457f735c1f78b33842325471a15cd997
Aaron <rudkowsk@uni-bremen.de>
2020-08-02 20:31:32 +0200

    weaponcontroller

--- core/src/com/galaxytrucker/galaxytruckerreloaded/Test/Client/WeaponControllerTest.java
+++ core/src/com/galaxytrucker/galaxytruckerreloaded/Test/Client/WeaponControllerTest.java
@@ -14,6 +14,8 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.UUID;
+
 public class WeaponControllerTest {
     /**
      * setup
@@ -56,6 +58,42 @@ public class WeaponControllerTest {
     }
 
     /**
+     * try unequipping a weapon already in inventory
+     */
+    @Test
+    public void unequipUnequipped() {
+        WeaponController controller = WeaponController.getInstance(null);
+
+        Weapon w = new Weapon();
+        Ship s = ClientControllerCommunicator.getInstance(null).getClientShip();
+        for(Room r : s.getSystems()) {
+            if(r.isSystem() && ((System)r).getSystemType() == SystemType.WEAPON_SYSTEM) {
+                w = ((System) r).getShipWeapons().get(0);
+                break;
+            }
+        }
+
+        controller.unequipWeapon(w);
+
+        boolean success = controller.unequipWeapon(w);
+        Assert.assertFalse(success);
+    }
+
+
+    /**
+     * try unequipping unknown weapon
+     */
+    @Test
+    public void unequipStrangeWeapon() {
+        WeaponController controller = WeaponController.getInstance(null);
+
+        Weapon w = new Weapon();
+        w.setId(UUID.randomUUID().hashCode());
+        boolean success = controller.unequipWeapon(w);
+        Assert.assertFalse(success);
+    }
+
+    /**
      * equip a weapon
      */
     @Test
@@ -86,5 +124,39 @@ public class WeaponControllerTest {
                 break;
             }
         }
+    }
+
+    /**
+     * try equipping an equipped weapon
+     */
+    @Test
+    public void equipEquipped() {
+        WeaponController controller = WeaponController.getInstance(null);
+
+        Weapon w = new Weapon();
+        Ship s = ClientControllerCommunicator.getInstance(null).getClientShip();
+        for(Room r : s.getSystems()) {
+            if(r.isSystem() && ((System)r).getSystemType() == SystemType.WEAPON_SYSTEM) {
+                w = ((System) r).getShipWeapons().get(0);
+                break;
+            }
+        }
+
+        boolean success = controller.equipWeapon(w);
+        Assert.assertFalse(success);
+    }
+
+    /**
+     * try equipping strange weapon
+     */
+    @Test
+    public void equipStrangeWeapon() {
+        WeaponController controller = WeaponController.getInstance(null);
+
+        Weapon w = new Weapon();
+        w.setId(UUID.randomUUID().hashCode());
+
+        boolean success = controller.equipWeapon(w);
+        Assert.assertFalse(success);
     }
 }
